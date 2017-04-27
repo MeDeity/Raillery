45c9e3f63a013cb3cd5856c8bbaff73cd54cd6f8
Deity <546024423@qq.com>
2017-04-27 12:14:46 +0800

    (+)新增欢迎界面

---
+++ app/src/main/java/com/deity/raillery/SplashActivity.java
@@ -0,0 +1,56 @@
+package com.deity.raillery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.deity.raillery.base.BaseActivity;
+
+import butterknife.BindView;
+
+/**
+ * Created by fengwenhua on 2017/4/27.
+ */
+
+public class SplashActivity extends BaseActivity {
+    @BindView(R.id.splash_resouce)
+    public ImageView splash_resouce;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startAnimation();
+    }
+
+    private void startAnimation(){
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.activity_splash);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(mainIntent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        splash_resouce.startAnimation(animation);
+    }
+
+
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_splash;
+    }
+}
